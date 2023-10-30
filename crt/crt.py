91d9eb1552050c5f003f2ef4018da33d8d65e835
Zak Hussain <79920023+Zak-Hussain@users.noreply.github.com>
2023-10-30 15:35:29 +0100

    commit

--- crt/crt.py
+++ crt/crt.py
@@ -5,10 +5,12 @@ from transformers import (
 # Load the model and tokenizer
 model_ckpt = "TheBloke/Llama-2-13B-chat-GPTQ"
 model = AutoModelForCausalLM.from_pretrained(
-    model_ckpt, device_map="auto", trust_remote_code=False,
-    revision="main"
+    model_ckpt, device_map="auto",
+    trust_remote_code=False, revision="main"
 )
-tokenizer = AutoTokenizer.from_pretrained(model_ckpt, use_fast=True)
+tokenizer = AutoTokenizer.from_pretrained(
+    model_ckpt, use_fast=True
+)
 
 # Load the generator pipeline
 generator = pipeline(
@@ -27,13 +29,15 @@ prompt = (
     "3. In a lake, there is a patch of lily pads. "
     "Every day, the patch doubles in size. "
     "If it takes 48 days for the patch to cover the entire lake, "
-    "how long would it take for the patch to cover half of the lake?\n"
+    "how long would it take for the patch to cover half of the "
+    "lake?\n"
 )
 prompt_template = (
     f"[INST] <<SYS>>\n"
-    "You are about to participate in a psychology experiment with three questions. "
-    "Please take your time to consider your answer to each question, "
-    "and provide a short answer.\n"
+    "You are about to participate in a psychology experiment "
+    "with three questions. "
+    "Please take your time to consider your answer to each "
+    "question, and provide a short answer.\n"
     f"<</SYS>>\n{prompt}[/INST]\n"
 )
 
